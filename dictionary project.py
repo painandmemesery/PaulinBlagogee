66b5dd8dfaf134c17750623bf0dca9044eee31c6
Kwame paulin Saviour Blagogee <kwamepaulin.blagogee@stu.jefferson.kyschools.us>
2018-03-19 08:21:47 -0400

    Paulin's dictionary project

---
+++ dictionary project.py
@@ -0,0 +1,16 @@
+import pprint
+twitch_lingo = {
+    'poggers': ("It is used in place of PogChamp as an expression of extreme excitement or hype, particularly after an impressive play is made."),
+    
+    'monkaS': ("MonkaS is a Better Twitch TV emote featuring an illustration of Pepe the Frog appearing frightened while sweating, which is typically used in Twitch chat during moments of high tension in video game matches to express anxiety."),
+    
+    'feelsgoodman': (" The phrase is in reference to the emotions one of the characters in the strip feels when he goes to the bathroom standing up, with his pants all the way down."),
+    
+    'feelsbadman': ("It is used to denote feelings of failure or disappointment, either by posting the image or using the phrase feelsbadman.jpg."),
+    
+    'kkona': ("Whenever there's a reference to something extremely white or redneck-like.When there's a reference to hats. It is one of the few emotes with a hat. KKona itself doesn't really mean anything."),
+    
+    'biblethump': ("BibleThump is a graphic emoticon featuring the face of the character Isaac from the video game The Binding of Isaac, which is commonly used to convey a feeling of sadness in various chats hosted on the live streaming video platform Twitch."),
+    }
+print(twitch_lingo)
+
